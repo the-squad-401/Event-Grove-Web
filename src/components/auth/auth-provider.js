f8ffb90e67ff73f24fb2d87d770912e702fad0b0
Jon <schwamman@msn.com>
2019-11-17 16:27:57 -0600

    Created auth provider

---
+++ src/components/auth/auth-provider.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import cookie from 'react-cookies';
+import jwt from 'jsonwebtoken';
+import superagent from 'superagent';
+
+import LoginContext from './login-context';
+
+const URL = process.env.REACT_APP_API
+
+export default class LoginProvider extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      token: null,
+      user: null,
+      login: this.login,
+      logout: this.logout,
+    };
+  }
+
+  login = (username, password) => {
+    superagent
+      .post(`${URL}/signin`)
+      .auth(username, password)
+        .then(token => this.verifyToken(token))
+        .catch(error => console.log(error));
+  }
+
+  logout = () => {
+    this.setLoginState(null, null);
+  }
+
+  verifyToken = token => {
+    try {
+      let user = jwt.decode(token);
+      this.setLoginState(token, user);
+      console.log('Logged in:', user)
+    } catch(error) {
+      this.logout();
+      console.log(error);
+    }
+  }
+
+  setLoginState = (token, user) => {
+    this.setState(token, user);
+    token ? cookie.save('auth', token) : cookie.remove('auth');
+  }
+
+  componentDidMount() {
+    const authToken = cookie.load('auth');
+    this.verifyToken(authToken);
+  }
+
+  render() {
+    return (
+      <LoginContext.Provider value={this.state}>
+        {this.props.children}
+      </LoginContext.Provider>
+    );
+  }
+}
