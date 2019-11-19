81f5484d512ff1612e9526fc45edf0f0674de075
Jacob-Wendt <jacob.wendt@yhaoo.com>
2019-11-19 14:24:49 -0600

    about us added!

--- src/pages/about-page/about.js
+++ src/pages/about-page/about.js
@@ -1,20 +1,72 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Card, Button, CardGroup } from 'react-bootstrap';
+
+import "./about.scss";
 
 export default function About() {
   return (
-    <section>
-      <Container>
-        <Row>
-          <Col>1 of 2</Col>
-          <Col>2 of 2</Col>
-        </Row>
-        <Row>
-          <Col>1 of 3</Col>
-          <Col>2 of 3</Col>
-          <Col>3 of 3</Col>
-        </Row>
-      </Container>
-    </section>
+    <section id='about-cards'>
+      <Card>
+        <Card.Img variant="top" src="https://ca.slack-edge.com/T3F4FFV5F-UEGP921EF-gcdbcd5b22a0-512" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="https://ca.slack-edge.com/T3F4FFV5F-UEGP921EF-gcdbcd5b22a0-512" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="https://ca.slack-edge.com/T3F4FFV5F-UEGP921EF-gcdbcd5b22a0-512" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="https://ca.slack-edge.com/T3F4FFV5F-UEGP921EF-gcdbcd5b22a0-512" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="https://ca.slack-edge.com/T3F4FFV5F-UEGP921EF-gcdbcd5b22a0-512" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+  </section>
+  
   );
 }
