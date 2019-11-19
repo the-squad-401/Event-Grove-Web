3bb7e7efd9696fc731177ea97d1ea943cbf6ccf4
Calvin G Hall <123chall@gmail.com>
2019-11-19 09:32:09 -0600

    Fixed carousel

--- src/components/carousels/business-carousel.js
+++ src/components/carousels/business-carousel.js
@@ -1,28 +1,25 @@
 import React from 'react';
 
-import Carousel from 'react-bootstrap/Carousel'
-
-import {useState} from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 
 export default function BussinessCarousel(props){
 
   
   return(
     <Carousel>
-      <Carousel.Item>
-        <img
-          className="bizCar"
-          src="src\assets\img\348s.jpg"
-          alt="First slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="bizCar"
-          src="src\assets\img\0ba5bead0a1e4b1ed557fdaea6dd50ee.jpg"
-          alt="Second slide"
-        />
-      </Carousel.Item>
+      {
+        props.images.map(({src, alt}) => {
+          return (
+            <Carousel.Item>
+              <img
+                className="carousel-card"
+                src={src}
+                alt={alt}
+              />
+            </Carousel.Item>
+          )
+        })
+      }
     </Carousel>
   )
 }
