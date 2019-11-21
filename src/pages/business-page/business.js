2e9dbb6cc189fcb7b9b272ff3dec3a4dcbdb2866
schwamman <schwamman@msn.com>
2019-11-21 12:38:33 -0600

    Disabled React errors

--- src/pages/business-page/business.js
+++ src/pages/business-page/business.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect, useContext } from 'react';
 import superagent from 'superagent';
 import {Button} from 'react-bootstrap';
